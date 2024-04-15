947f1d1e5b838735a8837f5a915a4b1e758a3f8b
crazydj8 <akshataryan8@gmail.com>
2024-04-15 10:32:32 +0530

    Started spring project

---
+++ src/main/java/com/ooadteam5/etlautomation/miniporject/MiniporjectApplication.java
@@ -0,0 +1,13 @@
+package com.ooadteam5.etlautomation.miniporject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniporjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniporjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ooadteam5/etlautomation/miniporject/MiniporjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ooadteam5.etlautomation.miniporject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniporjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
